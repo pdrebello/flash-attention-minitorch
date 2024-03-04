corrected softmax

minitorch/cuda_kernel_ops.py
@@ -98,12 +98,10 @@ class CudaKernelOps(TensorOps):
 
     @staticmethod
     def zip(fn: Callable[[float, float], float]) -> Callable[[Tensor, Tensor], Tensor]:
-        
         fn_id = fn_map[fn]
 
         def ret(a: Tensor, b: Tensor) -> Tensor:
             c_shape = shape_broadcast(a.shape, b.shape)
-            
             out = a.zeros(c_shape)
 
             lib.tensorZip.argtypes = [
@@ -129,16 +127,7 @@ class CudaKernelOps(TensorOps):
 
             # assert out.size == a.size, f"zip {out.size}, {a.size}"
             # assert out.size == b.size, f"zip {out.size}, {b.size}"
-            print(a.shape, b.shape)
-            if(b.shape == (1,)):
-                print("Here")
-                print(out.shape, a.shape, b.shape, fn_id)
-                print(b)
-                print(a._tensor._shape.astype(np.int32))
-                print(a._tensor._strides.astype(np.int32))
-                print(a.size)
-                print(b.size)
-                print("CALL ZIP")
+
             lib.tensorZip(
                 out._tensor._storage,
                 out._tensor._shape.astype(np.int32),
@@ -157,7 +146,6 @@ class CudaKernelOps(TensorOps):
                 len(b.shape),
                 fn_id
             )
-            print("Done")
             return out
 
         return ret
@@ -388,7 +376,6 @@ class CudaKernelOps(TensorOps):
     @staticmethod
     def attn_softmax_fw(inp: Tensor, mask: Tensor):
       batch_size, nhead, from_len, to_len = inp.shape
-      print(inp.shape)
       is_dec_self_attn = False
       stream = torch.cuda.current_stream().cuda_stream
 
@@ -414,6 +401,7 @@ class CudaKernelOps(TensorOps):
         is_dec_self_attn,
         stream
       ) 
+
       return inp
 
     @staticmethod
@@ -423,7 +411,7 @@ class CudaKernelOps(TensorOps):
       assert(out_grad.shape == soft_inp.shape)
       rows = batch_size * nhead * from_len
       softmax_len = to_len
-        
+      
       stream = torch.cuda.current_stream().cuda_stream
 
       lib_softmax.launch_attn_softmax_bw.argtypes = [
@@ -445,16 +433,13 @@ class CudaKernelOps(TensorOps):
       return out_grad
       #   END ASSIGN3_1
 
-
     @staticmethod
     def layernorm_fw(inp: Tensor, gamma: Tensor, beta: Tensor):
       #   BEGIN ASSIGN3_2
       batch_size, hidden_dim = inp.shape
-      #print(beta.shape)
       ln_res = inp.zeros((batch_size, hidden_dim))
       vars = minitorch.zeros((batch_size,))
       means = minitorch.zeros((batch_size,))
-      #print(means.shape)
       stream = torch.cuda.current_stream().cuda_stream
         
       lib_layernorm.launch_layernorm.argtypes = [
@@ -481,12 +466,54 @@ class CudaKernelOps(TensorOps):
         hidden_dim,
         stream
       ) 
-      return ln_res
+      return ln_res, vars, means
       #   END ASSIGN3_2
       
     @staticmethod
     def layernorm_bw(out_grad: Tensor, inp: Tensor, gamma: Tensor, beta: Tensor, var: Tensor, mean: Tensor):
       #   BEGIN ASSIGN3_2
-      raise("Not implemented")
+        batch_size, hidden_dim = inp.shape
+        gamma_grad = inp.zeros(gamma.shape)
+        betta_grad = inp.zeros(beta.shape)
+        inp_grad = inp.zeros(inp.shape)
+        
+        stream1 = torch.cuda.current_stream().cuda_stream
+        stream2 = torch.cuda.current_stream().cuda_stream
+        
+        lib_layernorm.launch_layernorm_bw.argtypes = [
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            np.ctypeslib.ndpointer(dtype=datatype, ndim=1, flags='C_CONTIGUOUS'),
+            ctypes.c_int,
+            ctypes.c_int,
+            ctypes.c_void_p,
+            ctypes.c_void_p
+        ]
+        
+        lib_layernorm.launch_layernorm_bw.restype = None
+        
+        lib_layernorm.launch_layernorm_bw(
+            gamma_grad._tensor._storage,
+            betta_grad._tensor._storage,
+            inp_grad._tensor._storage,
+            out_grad._tensor._storage,
+            inp._tensor._storage,
+            gamma._tensor._storage,
+            beta._tensor._storage,
+            var._tensor._storage,
+            mean._tensor._storage,
+            batch_size,
+            hidden_dim,
+            stream1,
+            stream2
+        )
+
+        return inp_grad, gamma_grad, betta_grad
       #   END ASSIGN3_2
       

minitorch/nn.py
@@ -209,8 +209,9 @@ def GELU(input: Tensor) -> Tensor:
     """Applies the GELU activation function with 'tanh' approximation element-wise
     https://pytorch.org/docs/stable/generated/torch.nn.GELU.html
     """
-    # COPY FROM ASSIGN2_2
-    raise NotImplementedError
+    ### BEGIN YOUR SOLUTION
+    return 0.5 * input * (1+(math.sqrt(2/math.pi) * (input + 0.044715 * (input**3))).tanh())
+    ### END YOUR SOLUTION
 
 
 def logsumexp(input: Tensor, dim: int) -> Tensor:
@@ -225,8 +226,11 @@ def logsumexp(input: Tensor, dim: int) -> Tensor:
         out : The output tensor with the same number of dimensions as input (equiv. to keepdims=True)
             NOTE: minitorch functions/tensor functions typically keep dimensions if you provide a dimensions.
     """  
-    # COPY FROM ASSIGN2_2
-    raise NotImplementedError
+    ### BEGIN YOUR SOLUTION
+    input_max = max(input, dim)
+    input = (input - input_max).exp().sum(dim).log()
+    return input_max + input
+    ### END YOUR SOLUTION
 
 
 def one_hot(input: Tensor, num_classes: int) -> Tensor:
@@ -236,8 +240,17 @@ def one_hot(input: Tensor, num_classes: int) -> Tensor:
 
     Hint: You may want to use a combination of np.eye, tensor_from_numpy, 
     """
-    # COPY FROM ASSIGN2_2
-    raise NotImplementedError
+    ### BEGIN YOUR SOLUTION
+    indices = input.to_numpy().flatten().astype(int)
+    one_hot = np.zeros((len(indices), num_classes))
+    one_hot[np.arange(len(indices)), indices] = 1
+    
+    output_shape = input.shape
+    output_shape = (*output_shape, num_classes
+                   )
+    one_hot = one_hot.reshape(output_shape)
+    return tensor_from_numpy(one_hot, backend=input.backend) 
+    ### END YOUR SOLUTION
 
 
 def softmax_loss(logits: Tensor, target: Tensor) -> Tensor:
@@ -252,8 +265,10 @@ def softmax_loss(logits: Tensor, target: Tensor) -> Tensor:
         loss : (minibatch, )
     """
     result = None
-    
-    # COPY FROM ASSIGN2_2
-    raise NotImplementedError
-    
+    ### BEGIN YOUR SOLUTION
+    batch_size = logits.shape[0]
+    num_classes = logits.shape[1]
+    one_hot_target = one_hot(target, num_classes)
+    result = logsumexp(logits, dim=1) -  (one_hot_target * logits).sum(dim=1)
+    ### END YOUR SOLUTION
     return result.view(batch_size, )